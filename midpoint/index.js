17e2552c649194c36a578b6250be6d90eb4b38cc
Petko Kostadinov <petkoxray@gmail.com>
2018-03-15 14:55:21 +0200

    more solutions :)

---
+++ midpoint/index.js
@@ -0,0 +1,41 @@
+// --- Directions
+// Return the 'middle' node of a linked list.
+// If the list has an even number of elements, return
+// the node at the end of the first half of the list.
+// *Do not* use a counter variable, *do not* retrieve
+// the size of the list, and only iterate
+// through the list one time.
+// --- Example
+//   const l = new LinkedList();
+//   l.insertLast('a')
+//   l.insertLast('b')
+//   l.insertLast('c')
+//   midpoint(l); // returns { data: 'b' }
+
+const L = require('./linkedlist');
+const LinkedList = L.LinkedList;
+
+function midpoint(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
+
+module.exports = midpoint;
+
+  const l = new LinkedList();
+  l.insertLast('a')
+  l.insertLast('b')
+  l.insertLast('c')
+  l.insertLast('1')
+  l.insertLast('2')
+  l.insertLast('3')
+  
+  let result = midpoint(l); // returns { data: 'b' }
+  console.log(result);
